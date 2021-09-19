f3a56afb9e7f7b5012406dde7c46a6f8f9ff1643
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-09-19 16:37:12 -0300

    added function to create a new user

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailAlreadyRegistered = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyRegistered) {
+      throw new Error("Email already registered");
+    }
+
+    const createdUser = this.usersRepository.create({ name, email });
+
+    return createdUser;
   }
 }
 
